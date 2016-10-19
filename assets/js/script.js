093f5a0234cf8786c4e32906593f696198f44215
Lani Huang <laniywh@gmail.com>
2016-10-19 11:10:16 -0700

    Typography changes, UX improvement, contact page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
-  $("header").headroom();
+  $("#header").headroom();
 
   $('#toggle').click(function() {
     $(this).toggleClass('active');
+    // Show nav
+    $('#overlay').toggle();
+    // Apply overlay style to nav
+    $('#overlay').addClass('isOverlay');
+    // Open overlay
     $('#overlay').toggleClass('open');
+  });
+
+  $(window).resize(function(){
+    if($(window).width() > 535) {
+      // Remove overlay style
+      $('#overlay').removeClass('isOverlay');
+      // Show nav
+      $('#overlay').removeAttr('style');
+      // Close menu
+      $('#toggle').removeClass('active');
+      // Close overlay
+      $('#overlay').removeClass('open');
+    }
+
+    if($(window).width() < 535) {
+      // Apply overlay style
+      $('#overlay').addClass('isOverlay');
+    }
   });
 
 });
