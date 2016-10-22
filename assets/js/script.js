b799420dee77611398a4b80d4dae333f0a73a7b2
Lani Huang <laniywh@gmail.com>
2016-10-21 18:11:56 -0700

    Use two nav for desktop and mobile nav menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,15 @@ $(document).ready(function(){
 
   $('#toggle').click(function() {
     $(this).toggleClass('active');
-    // Show nav
-    $('#overlay').toggle();
-    // Apply overlay style to nav
-    $('#overlay').addClass('isOverlay');
-    // Open overlay
     $('#overlay').toggleClass('open');
   });
 
   $(window).resize(function(){
     if($(window).width() >= 550) {
-      // Remove overlay style
-      $('#overlay').removeClass('isOverlay');
-      // Show nav
-      $('#overlay').removeAttr('style');
       // Close menu
       $('#toggle').removeClass('active');
       // Close overlay
       $('#overlay').removeClass('open');
-    }
-
-    if($(window).width() < 550) {
-      // Apply overlay style
-      $('#overlay').addClass('isOverlay');
     }
   });
 
