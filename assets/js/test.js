8d8ba905ba4df5641771521570439cf8b9e67f2f
Lani Huang <laniywh@gmail.com>
2016-12-27 00:07:26 -0800

    Site updated at 2016-12-27 00:07:26 -0800

---
+++ assets/js/test.js
@@ -0,0 +1,13 @@
+// external js: masonry.pkgd.js
+
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  masonry: {
+    // isFitWidth: true,
+    columnWidth: 150,
+    gutter: 20,
+  }
+  // itemSelector: '.grid-item',
+  // columnWidth: 160,
+  // gutter: 20
+});
